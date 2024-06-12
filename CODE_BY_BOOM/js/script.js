09bfc6b4877db2ab28e5a51fa973f831b35c9132
Boombm <nutruja.bm@gmail.com>
2024-06-13 02:15:36 +0700

    Edit Home page and Login page

---
+++ CODE_BY_BOOM/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('#login-btn').addEventListener('click', function() {
+    location.href = 'login.html';
+});
+document.getElementById("login-link").addEventListener("click", function() {
+    location.href = 'login.html';
+  });
